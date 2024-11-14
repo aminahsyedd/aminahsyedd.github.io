3bd4c578e50943dfb33ba2504dde1910ec7fa0be
aminahsyedd <aminah.syed@torontomu.ca>
2024-11-14 01:14:31 -0500

    updated pages

--- js/main.js
+++ js/main.js
@@ -31,3 +31,19 @@ document.getElementById('nameButton').addEventListener('click', function() {
         document.getElementById('greeting').textContent = "Hello! Welcome to my website.";
     }
 });
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
